85e58239109cb8e1ce121a37064a87d79b1ffa2e
Shortarm <cstreet@mighty.co.za>
2017-03-29 15:33:10 +0200

    added reset button

--- script.js
+++ script.js
@@ -1,11 +1,14 @@
 $(document).ready(function() {
 	var i = 0
   var cell = '<div class = "cell"> </div>';
-  for (var i = 0; i < 256; i++){
+  for (var i = 0; i < 16*16; i++){
 	$("#wrapper").append(cell);
 
 };
 $(".cell").mouseenter(function(){
     $(this).css("background-color", "yellow");
+});
+$("#reset").click(function(){
+  document.location.reload(true);
 });
 });
