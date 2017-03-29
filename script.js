8c1f690b1d37afbb088f19371d6201e83c79602d
Shortarm <cstreet@mighty.co.za>
2017-03-29 14:38:08 +0200

    added mouseenter trail

--- script.js
+++ script.js
@@ -3,5 +3,9 @@ $(document).ready(function() {
   var cell = '<div class = "cell"> </div>';
   for (var i = 0; i < 256; i++){
 	$("#wrapper").append(cell);
-}
+
+};
+$(".cell").mouseenter(function(){
+    $(this).css("background-color", "yellow");
+});
 });
