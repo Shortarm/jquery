1fc93319b6ef94b5a62f9eaf9f943fc9530ad6d4
Shortarm <cstreet@mighty.co.za>
2017-03-31 12:42:00 +0200

    added prompt and code for user input grid size

--- script.js
+++ script.js
@@ -1,14 +1,25 @@
 $(document).ready(function() {
-	var i = 0
-  var cell = '<div class = "cell"> </div>';
-  for (var i = 0; i < 16*16; i++){
+	var i = 0;
+	//var s = 16;//
+	var square = prompt("Enter the number of cells per side you want.",16);
+	var s = square;
+	  var cell = '<div class = "cell" style="height:' + 672/s + 'px; width:' + 672/s + 'px;"> </div>';
+  for (var i = 0; i < s*s; i++){
 	$("#wrapper").append(cell);
-
 };
 $(".cell").mouseenter(function(){
     $(this).css("background-color", "yellow");
 });
 $("#reset").click(function(){
-  document.location.reload(true);
+
+	document.location.reload();
+	});
 });
-});
+/*function() {
+		if(672%square === 0) {
+		var s = 672/square;
+} else {
+console.log("It must be a whole number!");
+}
+	};
+} */
